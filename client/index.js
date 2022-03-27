7e058134c9f945604613f9c5bd44fe1f3f11c0b0
Jessica Hong <jeessicahong@gmail.com>
2022-03-27 00:33:22 -0700

    test method to ensure canvas is working

--- client/index.js
+++ client/index.js
@@ -4,6 +4,6 @@ console.log(game.hello_world())
 console.log(game.Game.default_config())
 
 let snakeGame = new game.Game(game.Game.default_config());
-
+snakeGame.start();
 console.log(snakeGame.config())
 console.log(snakeGame.snake())
