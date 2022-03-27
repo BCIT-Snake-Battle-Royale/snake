29df7b16d64d80e943bb9954550b0d335e7f9908
Jessica Hong <jeessicahong@gmail.com>
2022-03-27 00:32:31 -0700

    defined implementation on canvas

---
+++ libs/game-wasm/src/canvas.rs
@@ -0,0 +1,34 @@
+use web_sys::window;
+use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
+use wasm_bindgen::JsCast;
+
+pub struct Canvas {
+  pub canvas: HtmlCanvasElement,
+  pub ctx: CanvasRenderingContext2d,
+  width: u32,
+  height: u32
+}
+
+impl Canvas {
+  pub fn new(canvas_id: &str, width: u32, height: u32) -> Canvas {
+    let document = window().unwrap().document().unwrap();
+    let canvas = document.get_element_by_id(canvas_id).unwrap();
+    let canvas: HtmlCanvasElement = canvas
+      .dyn_into::<HtmlCanvasElement>()
+      .map_err(|_| ())
+      .unwrap();
+      
+    let ctx: CanvasRenderingContext2d = canvas
+      .get_context("2d")
+      .unwrap()
+      .unwrap()
+      .dyn_into::<CanvasRenderingContext2d>().unwrap();
+    
+    Self {
+      canvas,
+      ctx,
+      width,
+      height,
+    }
+  }
+}
