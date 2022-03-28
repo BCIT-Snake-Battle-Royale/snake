686ce759ce5323789ea420b3a59546f379fa9783
Maria <mariajfdavis@gmail.com>
2022-03-28 12:38:29 -0700

    Modify item model, add setters to snake model, add apply_item_effect to lib.rs (WIP)

--- libs/game/src/item.rs
+++ libs/game/src/item.rs
@@ -1,11 +1,16 @@
 use crate::*;
 
+enum ItemType {
+    SpeedModifier,
+    InvincibilityModifier,
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Item {
+    pub item_type: ItemType,
     pub x: usize,
     pub y: usize,
     pub speed_effect: i32,
-    pub invincibility: bool,
 }
 
 impl Item {
@@ -18,19 +23,20 @@ impl Item {
         }
     }
 
-    pub fn x(&self) -> usize {
+    pub fn get_x(&self) -> usize {
         self.x
     }
 
-    pub fn y(&self) -> usize {
+    pub fn get_y(&self) -> usize {
         self.y
     }
 
-    pub fn speed_effect(&self) -> i32 {
+    pub fn get_speed_effect(&self) -> i32 {
         self.speed_effect
     }
 
-    pub fn invincibility(&self) -> bool {
-        self.invincibility
+    pub fn get_item_type(&self) -> ItemType {
+        self.item_type
     }
+
 }
