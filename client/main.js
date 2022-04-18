bbc6e97164d214a33c5e233ace70cc6db9ef332e
Daniel Chung <danielchung93@gmail.com>
2022-04-17 21:59:49 -0700

    html, css, js for main landing page

---
+++ client/main.js
@@ -0,0 +1,31 @@
+// import { io } from 'socket.io-client';
+
+// const socket = io('http://localhost:4321');
+
+newGame = () => {
+    let name = document.getElementById("username").value;
+    if (name != "") {
+        // socket.emit('startGame', { username: name });
+        document.getElementById("username").style.display = 'none';
+        document.getElementById("host-game").style.display = 'none';
+        document.getElementById("room-code").style.display = 'none';
+        document.getElementById("join-game").style.display = 'none';
+    } else {
+        alert("Please enter a username to host a game.");
+    }
+}
+
+joinGame = () => {
+    let name = document.getElementById("username").value;
+    let code = document.getElementById("room-code").value;
+    if (name != "" && code != "") {
+        // socket.emit('joinGame', { username: name, 
+        //                           roomCode: code });
+        document.getElementById("username").style.display = 'none';
+        document.getElementById("host-game").style.display = 'none';
+        document.getElementById("room-code").style.display = 'none';
+        document.getElementById("join-game").style.display = 'none';
+    } else {
+        alert("Please enter a username and valid room code to join a game.");
+    }
+}
